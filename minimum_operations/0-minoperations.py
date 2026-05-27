fd734b1dcd07f8212ebd1657a599c73ef5bc2651
Melopheelo12 <ryan12052002@gmail.com>
2026-05-27 09:41:07 +0200

    t0

---
+++ minimum_operations/0-minoperations.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+"""Module that calculate the minimum number of operation
+needed to result in exacly n H character"""
+
+
+def minOperations(n):
+    """return the min number of operations
+    Args:
+        n (int): target number H
+    Returns:
+            int: min number
+    """
+    if not isinstance(n, int) or n < 2:
+        return 0
+
+    operations = 0
+    facteur = 2
+
+    while n < 1:
+        while n % facteur == 0:
+            operations += facteur
+            n //= facteur
+        facteur += 1
+    return operations
