bb7298bd39f59d115f90cc8e818172c6178e4d85
Melopheelo12 <ryan12052002@gmail.com>
2026-07-09 14:51:58 +0200

    0-island_perimeter.py

--- island_perimeter/0-island_perimeter.py
+++ island_perimeter/0-island_perimeter.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+"""Module for computing the perimeter of an island."""
+
+
+def island_perimeter(grid):
+    """Return the perimeter of the island described in grid."""
+    if not grid or not grid[0]:
+        return 0
+
+    perimeter = 0
+    rows = len(grid)
+    cols = len(grid[0])
+
+    for row in range(rows):
+        for col in range(cols):
+            if grid[row][col] == 1:
+                perimeter += 4
+                if row > 0 and grid[row - 1][col] == 1:
+                    perimeter -= 2
+                if col > 0 and grid[row][col - 1] == 1:
+                    perimeter -= 2
+
+    return perimeter
